fd9556405ce41d1001ec5f3978155dd27bc1bfa2
Camape <cagrmape@gmail.com>
2017-04-17 10:17:55 +0100

    Ahora los paquetes salientes están identificados por TRPI

--- trpi.hpp
+++ trpi.hpp
@@ -125,7 +125,7 @@ class Trpi{
 							ytmp = moment.m01/area;
 							objectFound = true;
 							refArea = area;
-								output+=""+intToString((double)(ytmp-y)/10)+","+intToString((areaant-area)/100.00)+",1/"+"\n";
+								output+=""+intToString((double)(ytmp-y)/10)+","+intToString((areaant-area)/100.00)+",1,TRPI/"+"\n";
 							
 							y=ytmp;
 							x=xtmp;
@@ -171,7 +171,7 @@ class Trpi{
 			if (!symbols[0].img.data)
 				return -1;
 			threshold(symbols[0].img, symbols[0].img, 100, 255, 0);
-			symbols[0].name = "-0.5,0.0,1/\n";//Izquierda 90 grados = -0.5,0.0,0/
+			symbols[0].name = "-0.5,0.0,1,TRPI/\n";//Izquierda 90 grados = -0.5,0.0,0/
 
 			symbols[1].img = imread("arrowT.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 			cout<<".";
@@ -185,21 +185,21 @@ class Trpi{
 			if (!symbols[2].img.data)
 				return -1;
 			threshold(symbols[2].img, symbols[2].img, 100, 255, 0);
-			symbols[2].name = "-0.5,-0.5,1/\n";//Izquierda 45 grados = -0.5,0.5,0/
+			symbols[2].name = "-0.5,-0.5,1,TRPI/\n";//Izquierda 45 grados = -0.5,0.5,0/
 
 			symbols[3].img = imread("arrowR45.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 			cout<<".";
 			if (!symbols[3].img.data)
 				return -1;
 			threshold(symbols[3].img, symbols[3].img, 100, 255, 0);
-			symbols[3].name = "0.5,0.5,1/\n";//Derecha 45 grados = 0.5,0.5,0/
+			symbols[3].name = "0.5,0.5,1,TRPI/\n";//Derecha 45 grados = 0.5,0.5,0/
 
 			symbols[4].img = imread("arrowStop.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 			cout<<".";
 			if (!symbols[4].img.data)
 				return -1;
 			threshold(symbols[4].img, symbols[4].img, 100, 255, 0);
-			symbols[4].name = "0.0,0.0,0/\n";//STOP = 0.0,0.0,1/
+			symbols[4].name = "0.0,0.0,0,TRPI/\n";//STOP = 0.0,0.0,1/
 
 			symbols[5].img = imread("arrowGo.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 			cout<<".";
@@ -213,7 +213,7 @@ class Trpi{
 			if (!symbols[6].img.data)
 				return -1;
 			threshold(symbols[6].img, symbols[6].img, 100, 255, 0);
-			symbols[6].name = "0.5,0.0,1/\n"; //Derecha 90 grados = 0.5,0.0,0/
+			symbols[6].name = "0.5,0.0,1,TRPI/\n"; //Derecha 90 grados = 0.5,0.0,0/
 			cout<<" Done."<<endl;
 
 			return 0;
@@ -354,7 +354,7 @@ class Trpi{
 			string outputSymbols = checkSymbols(symbols,camera);
 			string outputObject = trackFilteredObject(x,y,thresholded,camera);
 
-			if (outputObject!=std::string() && outputSymbols!="0.0,0.0,0/\n"){ //GUÍA a no ser que haya una señal STOP
+			if (outputObject!=std::string() && outputSymbols!="0.0,0.0,0,TRPI/\n"){ //GUÍA a no ser que haya una señal STOP
 					timeAnt = clock();
 					return outputObject;
 			}else	if(outputSymbols!=std::string()){ //SI NO => SIGNO
